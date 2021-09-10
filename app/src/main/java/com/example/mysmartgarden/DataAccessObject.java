b9a1158300613fddb84c780f5492abcc2e83bc2a
na0 <koonayoung51@naver.com>
2021-09-10 18:58:52 +0900

    [ADD] 데이터베이스

---
+++ app/src/main/java/com/example/mysmartgarden/DataAccessObject.java
@@ -0,0 +1,28 @@
+package com.example.mysmartgarden;
+
+public class DataAccessObject {
+    private String name;
+    private String species;
+
+    // 생성자 초기화
+    public DataAccessObject(String name, String species) {
+        this.name = name;
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String data) {
+        this.species = species;
+    }
+}
