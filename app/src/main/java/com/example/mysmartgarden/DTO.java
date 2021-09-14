36752e2374d68fa07b1ef9010c858d53debf327f
na0 <koonayoung51@naver.com>
2021-09-14 19:32:42 +0900

    [RENAME + ADD] 줄임말, ip 입력 생성자 추가

--- app/src/main/java/com/example/mysmartgarden/DTO.java
+++ app/src/main/java/com/example/mysmartgarden/DTO.java
@@ -1,13 +1,15 @@
 package com.example.mysmartgarden;
 
-public class DataAccessObject {
+public class DTO {
     private String name;
     private String species;
+    private String ip;
 
     // 생성자 초기화
-    public DataAccessObject(String name, String species) {
+    public DTO(String name, String species, String ip) {
         this.name = name;
         this.species = species;
+        this.ip = ip;
     }
 
     public String getName() {
@@ -22,7 +24,11 @@ public class DataAccessObject {
         return species;
     }
 
-    public void setSpecies(String data) {
+    public void setSpecies(String species) {
         this.species = species;
     }
+
+    public String getIp(){ return ip; }
+
+    public void setIp(String ip){ this.ip = ip; }
 }
