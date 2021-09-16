51713f05594def36085fc2219efd1a6adaf86d22
na0 <koonayoung51@naver.com>
2021-09-16 20:37:16 +0900

    [FIX] 디바이스 인식해서 처음, 아닌지 확인하기

--- app/src/main/java/com/example/mysmartgarden/PlantSpinerActivity.java
+++ app/src/main/java/com/example/mysmartgarden/PlantSpinerActivity.java
@@ -1,10 +1,13 @@
 package com.example.mysmartgarden;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -22,16 +25,24 @@ import java.util.List;
 
 public class PlantSpinerActivity extends AppCompatActivity {
 
+
+    private DAO DAO = new DAO();
     List<String> categories = new ArrayList<String>();
+    private EditText editText;
+    private Spinner plant_spinner;
+    private String plant_species;
+    private String plant_name;
+    private String plant_ip;
+    private String DeviceID;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_app_activity);
 
-
         // Spinner 객체 생성
-        final Spinner spiner = (Spinner) findViewById(R.id.spinner_plant);
+        final Spinner spiner = (Spinner) findViewById(R.id.ip_spinner_plant);
 
         // 인자로 어댑처 생성
         final String[] string = getResources().getStringArray(R.array.plant_array);
@@ -43,7 +54,7 @@ public class PlantSpinerActivity extends AppCompatActivity {
         spiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "Selected Country: "+ spiner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Selected : "+ spiner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -51,6 +62,27 @@ public class PlantSpinerActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    //onclick event 메서드
+    public void onclick(View view){
+
+        DeviceID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        plant_spinner = (Spinner)findViewById(R.id.ip_spinner_plant);
+        plant_species = plant_spinner.getSelectedItem().toString();
+
+        editText = (EditText)findViewById(R.id.input_plant_name);
+        plant_name = editText.getText().toString();
+
+        editText = (EditText)findViewById(R.id.input_ip);
+        plant_ip = editText.getText().toString();
+
+        // 데이터를 새로 입력받고, 문서로 데이터를 저장해준다.
+        DAO.SaveUserID("user", DeviceID, plant_name, plant_species, plant_ip);
+
+        intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/mysmartgarden/introActivity.java
+++ app/src/main/java/com/example/mysmartgarden/introActivity.java
@@ -5,38 +5,81 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.DecimalFormat;
 
 public class introActivity extends AppCompatActivity {
 
     private DAO DAO = new DAO();
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
     private boolean isNewer;
     private String DeviceID;
     private Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro); //xml , java 소스 연결
         Handler handler = new Handler();
+        DeviceID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        //isNewer = DAO.isUserExist(DeviceID);
+
+        CollectionReference productRef = db.collection("user");
+        //get()을 통해서 해당 컬렉션의 정보를 가져온다.
+        productRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(Task<QuerySnapshot> task) {
+                //작업이 성공적으로 마쳤을때
+                isNewer = false;
+                if (task.isSuccessful()) {
+                    //컬렉션 아래에 있는 모든 정보를 가져온다.
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.v("test", "===========!!!===========");
+                        Log.v("test", DeviceID);
+                        Log.v("test", document.getId());
+                        if((document.getId().toString()).equals(DeviceID)){
+                            // Log.v("test", document.getData().toString());
+                            Log.v("test", "?!?!?!?!?!");
+                            isNewer = true;
+                            Log.v("test", "======================news???");
+                            Log.v("test", String.valueOf(isNewer));
+                        }
+                    }
+                } else { }
+            }
+        });
+        Log.v("test", "======================news?");
+        Log.v("test", String.valueOf(isNewer));
+
         handler.postDelayed(new Runnable(){
             @Override
             public void run() {
-                DeviceID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-                isNewer = DAO.isUserExist(DeviceID);
+                //isNewer = DAO.isUserExist(DeviceID);
+                Log.v("test", String.valueOf(isNewer));
+
+
                 // ture라면 DeviceID가 이미 있는 것, false라면 없는 것
                 if(!isNewer){ // 만약에 디바이스가 처음 등록하는 것이라면?
                     // 처음에 이름이 비어있다면, 이름과 식물의 종을 정하는 페이지로 감
+                    Log.v("test", "is first");
                     intent = new Intent (getApplicationContext(), PlantSpinerActivity.class);
-
-                    // 데이터를 새로 입력받고, 문서로 데이터를 저장해준다.
-                    DAO.SaveUserID("user", DeviceID, "난용이", "상추", "123.21.32.123");
                 }
                 else{
                     // 인트로 실행 후 바로 MainActivity로 넘어감.
+                    Log.v("test", "is not first");
                     intent = new Intent (getApplicationContext(), MainActivity.class);
                 }
                 startActivity(intent);
@@ -50,4 +93,38 @@ public class introActivity extends AppCompatActivity {
         super.onPause();
         finish();
     }
+
 }
+
+/*
+       CollectionReference productRef = db.collection("user");
+            //get()을 통해서 해당 컬렉션의 정보를 가져온다.
+            productRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(Task<QuerySnapshot> task) {
+                //작업이 성공적으로 마쳤을때
+                isNewer = false;
+                if (task.isSuccessful()) {
+                    //컬렉션 아래에 있는 모든 정보를 가져온다.
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        //document.getData() or document.getId() 등등 여러 방법으로
+                        // 만약에 디바이스가 있다면, userExistFlag를 ture로
+
+                        Log.v("test", "===========!!!===========");
+                        Log.v("test", DeviceID);
+                        Log.v("test", document.getId());
+
+                        if((document.getId().toString()).equals(DeviceID)){
+                           // Log.v("test", document.getData().toString());
+                            Log.v("test", "?!?!?!?!?!");
+                            isNewer = true;
+                        }
+
+                    }
+                    //그렇지 않을때
+                } else { }
+            }
+        });
+        Log.v("test", "======================news?");
+        Log.v("test", String.valueOf(isNewer));
+ */
