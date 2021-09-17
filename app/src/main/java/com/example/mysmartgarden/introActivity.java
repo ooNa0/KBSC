7883c48f5903d59060e1fda9a3ec9c88b7a5bd1f
na0 <koonayoung51@naver.com>
2021-09-17 14:47:48 +0900

    [UPDATE] 처음에 로딩 화면 시간지정(handler) 방식 -> 로딩 후 메인 페이지로 가는 방식 업데이트

--- app/src/main/java/com/example/mysmartgarden/introActivity.java
+++ app/src/main/java/com/example/mysmartgarden/introActivity.java
@@ -33,9 +33,8 @@ public class introActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro); //xml , java 소스 연결
-        Handler handler = new Handler();
+
         DeviceID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-        //isNewer = DAO.isUserExist(DeviceID);
 
         CollectionReference productRef = db.collection("user");
         //get()을 통해서 해당 컬렉션의 정보를 가져온다.
@@ -47,49 +46,55 @@ public class introActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     //컬렉션 아래에 있는 모든 정보를 가져온다.
                     for (QueryDocumentSnapshot document : task.getResult()) {
-                        Log.v("test", "===========!!!===========");
-                        Log.v("test", DeviceID);
-                        Log.v("test", document.getId());
-                        if((document.getId().toString()).equals(DeviceID)){
-                            // Log.v("test", document.getData().toString());
-                            Log.v("test", "?!?!?!?!?!");
+                        if ((document.getId().toString()).equals(DeviceID)) {
                             isNewer = true;
-                            Log.v("test", "======================news???");
-                            Log.v("test", String.valueOf(isNewer));
                         }
                     }
-                } else { }
-            }
-        });
-        Log.v("test", "======================news?");
-        Log.v("test", String.valueOf(isNewer));
-
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                //isNewer = DAO.isUserExist(DeviceID);
-                Log.v("test", String.valueOf(isNewer));
-
+                }
 
                 // ture라면 DeviceID가 이미 있는 것, false라면 없는 것
-                if(!isNewer){ // 만약에 디바이스가 처음 등록하는 것이라면?
+                if (!isNewer) { // 만약에 디바이스가 처음 등록하는 것이라면?
                     // 처음에 이름이 비어있다면, 이름과 식물의 종을 정하는 페이지로 감
-                    Log.v("test", "is first");
-                    intent = new Intent (getApplicationContext(), PlantSpinerActivity.class);
-                }
-                else{
+                    intent = new Intent(getApplicationContext(), PlantSpinerActivity.class);
+                    // this -> Activity context
+                    // getApplicationContext() -> Application context
+                } else {
                     // 인트로 실행 후 바로 MainActivity로 넘어감.
-                    Log.v("test", "is not first");
-                    intent = new Intent (getApplicationContext(), MainActivity.class);
+                    intent = new Intent(getApplicationContext(), MainActivity.class);
                 }
+                intent.putExtra("state", "launch");
                 startActivity(intent);
                 finish();
             }
-        },1500); //1초 후 인트로 실행
+        });
+
+
+//        handler.postDelayed(new Runnable(){
+//            @Override
+//            public void run() {
+//                //isNewer = DAO.isUserExist(DeviceID);
+//                Log.v("test", String.valueOf(isNewer));
+//
+//
+//                // ture라면 DeviceID가 이미 있는 것, false라면 없는 것
+//                if(!isNewer){ // 만약에 디바이스가 처음 등록하는 것이라면?
+//                    // 처음에 이름이 비어있다면, 이름과 식물의 종을 정하는 페이지로 감
+//                    Log.v("test", "is first");
+//                    intent = new Intent (getApplicationContext(), PlantSpinerActivity.class);
+//                }
+//                else{
+//                    // 인트로 실행 후 바로 MainActivity로 넘어감.
+//                    Log.v("test", "is not first");
+//                    intent = new Intent (getApplicationContext(), MainActivity.class);
+//                }
+//                startActivity(intent);
+//                finish();
+//            }
+//        },1500); //1초 후 인트로 실행
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         finish();
     }
